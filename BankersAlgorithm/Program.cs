4322e1dc4274e0d71d10fa3889c989772684640a
ShanaBlaizeHull <S.Blaize-2024@hull.ac.uk>
2026-05-10 21:39:25 +0100

    commit now

---
+++ BankersAlgorithm/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+class BankersAlgorithm
+{
+static void Main()
+{
+Console.Write("Number of processes: ");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Number of resource types: ");
+int m = int.Parse(Console.ReadLine());
+
+int[,] allocation = new int[n, m];
+int[,] maxDemand = new int[n, m];
+int[] available = new int[m];
+
+Console.WriteLine("Enter allocation matrix:");
+for (int i = 0; i < n; i++)
+{
+Console.Write($"P{i + 1}: ");
+string[] row = Console.ReadLine().Split(' ');
+for (int j = 0; j < m; j++)
+allocation[i, j] = int.Parse(row[j]);
+}
+
+Console.WriteLine("Enter maximum demand matrix:");
+for (int i = 0; i < n; i++)
+{
+Console.Write($"P{i + 1}: ");
+string[] row = Console.ReadLine().Split(' ');
+for (int j = 0; j < m; j++)
+maxDemand[i, j] = int.Parse(row[j]);
+}
+
+Console.Write("Enter available resources: ");
+string[] avail = Console.ReadLine().Split(' ');
+for (int j = 0; j < m; j++)
+available[j] = int.Parse(avail[j]);
+
+int[,] need = new int[n, m];
+for (int i = 0; i < n; i++)
+for (int j = 0; j < m; j++)
+need[i, j] = maxDemand[i, j] - allocation[i, j];
+
+bool[] finished = new bool[n];
+int[] work = (int[])available.Clone();
+List<int> safeSeq = new List<int>();
+
+int count = 0;
+while (count < n)
+{
+bool found = false;
+for (int i = 0; i < n; i++)
+{
+if (!finished[i])
+{
+bool canAllocate = true;
+for (int j = 0; j < m; j++)
+if (need[i, j] > work[j])
+{ canAllocate = false; break; }
+
+if (canAllocate)
+{
+for (int j = 0; j < m; j++)
+work[j] += allocation[i, j];
+
+finished[i] = true;
+safeSeq.Add(i + 1);
+found = true;
+count++;
+}
+}
+}
+if (!found) break;
+}
+
+if (safeSeq.Count == n)
+{
+Console.Write("Safe Sequence: ");
+for (int k = 0; k < safeSeq.Count; k++)
+{
+Console.Write($"P{safeSeq[k]}");
+if (k < safeSeq.Count - 1) Console.Write(" -> ");
+}
+Console.WriteLine(" System is in a safe state.");
+}
+else
+{
+Console.WriteLine("System is NOT in a safe state.");
+}
+}
+}
---
+++ TLBSimulation/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+class TLBEntry {
+    public int VPN;
+    public int PPN;
+}
+
+class TLB {
+    private List<TLBEntry> entries = new List<TLBEntry>();
+    private const int capacity = 4;
+    private int hits = 0;
+    private int misses = 0;
+
+    public void Translate(int vpn, int[] pageTable) {
+        int? ppn = Lookup(vpn);
+
+        if (ppn != null) {
+            hits++;
+            Console.WriteLine($"VPN {vpn} -> TLB HIT -> PPN {ppn}");
+        } else {
+            misses++;
+            int newPPN = pageTable[vpn];
+            Insert(vpn, newPPN);
+            Console.WriteLine($"VPN {vpn} -> TLB MISS -> PPN {newPPN} (loaded from page table)");
+        }
+    }
+
+    private int? Lookup(int vpn) {
+        foreach (var entry in entries) {
+            if (entry.VPN == vpn) return entry.PPN;
+        }
+        return null;
+    }
+
+    private void Insert(int vpn, int ppn) {
+        if (entries.Count >= capacity)
+            entries.RemoveAt(0);
+        entries.Add(new TLBEntry { VPN = vpn, PPN = ppn });
+    }
+
+    public void PrintHitRatio() {
+        int total = hits + misses;
+        double ratio = (double)hits / total * 100;
+        Console.WriteLine($"\nTotal: {total} | Hits: {hits} | Misses: {misses}");
+        Console.WriteLine($"Hit Ratio: {ratio:F1}%");
+    }
+}
+
+class Program {
+        static void Main() {
+            // Page table: VPN -> PPN
+            int[] pageTable = { 7, 9, 0, 5, 5, 3, 2, 4, 1, 6, 3, 8, 4, 2, 6, 1 };
+
+            TLB tlb = new TLB();
+
+            Console.WriteLine("=== TLB Simulation ===\n");
+
+         // Sequence of virtual page accesses
+            int[] accessSequence = { 0, 1, 2, 3, 0, 1, 6, 2, 3, 0 };
+
+            foreach (int vpn in accessSequence) {
+                tlb.Translate(vpn, pageTable);
+            }
+
+            tlb.PrintHitRatio();
+        }
+}
